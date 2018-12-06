89736ee2843e18e8bdf345a5d18f8d917cf8dccb
Jason Hite <hite77@gmail.com>
2018-12-06 18:33:04 -0500

    Another star

--- lib/advent_three.dart
+++ lib/advent_three.dart
@@ -1,23 +1,60 @@
 findOverlaps(String squares) {
+  List<List<int>> fabricArray = generateMap(squares);
+
+  return countOverlap(fabricArray);
+}
+
+
+int notOverlapped(String squares) {
+  List<List<int>> fabricArray = generateMap(squares);
+
+  return everyPartNotOverlapped(squares, fabricArray);
+}
+
+int everyPartNotOverlapped(String squares, List<List<int>> fabricArray) {
+  for(final square in squares.split('\n')) {
+    List<int> coords = parseValues(square);
+    int claim = coords[0];
+    int left = coords[1];
+    int top = coords[2];
+    int width = coords[3];
+    int height = coords[4];
+
+
+    bool found = true;
+    for (int i=0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        if (fabricArray[left + i][top + j] != 1) {
+          found = false;
+        }
+      }
+    }
+    if (found) {
+      return claim;
+    }
+  }
+}
+
+List<List<int>> generateMap(String squares) {
   List<List<int>> fabricArray = new List();
   for(int i=0; i<1000; i++)
   {
-      fabricArray.add(new List<int>.filled(1000, 0));
+    fabricArray.add(new List<int>.filled(1000, 0));
   }
 
   for(final square in squares.split('\n')) {
     applySquares(square, fabricArray);
   }
-
-  return countOverlap(fabricArray);
+  return fabricArray;
 }
+
 
 void applySquares(String square, List<List<int>> fabricArray) {
   List<int> coords = parseValues(square);
-  int left = coords[0];
-  int top = coords[1];
-  int width = coords[2];
-  int height = coords[3];
+  int left = coords[1];
+  int top = coords[2];
+  int width = coords[3];
+  int height = coords[4];
 
   for (int i=0; i < width; i++) {
     for (int j=0; j < height; j++) {
@@ -43,9 +80,12 @@ int countOverlap(List<List<int>> fabricArray) {
   return countOverlap;
 }
 
+
+
 parseValues(String square) {
   List<String> split = square.trim().split(' ');
+  String claim = split[0].split('#')[1];
   List<String> leftTop = split[2].split(':')[0].split(',');
   List<String> widthHeight = split[3].split('x');
-  return [int.parse(leftTop[0]), int.parse(leftTop[1]), int.parse(widthHeight[0]), int.parse(widthHeight[1])];
+  return [int.parse(claim), int.parse(leftTop[0]), int.parse(leftTop[1]), int.parse(widthHeight[0]), int.parse(widthHeight[1])];
 }
--- test/advent_three_test.dart
+++ test/advent_three_test.dart
@@ -5,21 +5,31 @@ import './day_data.dart';
 
 void main() {
   test('test parse function', () {
-    expect(parseValues('#1 @ 1,3: 4x4'), [1,3,4,4]);
-    expect(parseValues('#2 @ 3,1: 4x4'), [3,1,4,4]);
-    expect(parseValues('#3 @ 5,5: 2x2'), [5,5,2,2]);
+    expect(parseValues('#1 @ 1,3: 4x4'), [1,1,3,4,4]);
+    expect(parseValues('#2 @ 3,1: 4x4'), [2,3,1,4,4]);
+    expect(parseValues('#3 @ 5,5: 2x2'), [3,5,5,2,2]);
   });
 
-
-  test('first example', () {
+  test('first example overlap', () {
     expect(findOverlaps("""
 #1 @ 1,3: 4x4
 #2 @ 3,1: 4x4
 #3 @ 5,5: 2x2"""), 4);
   });
 
-  test('dataset', () {
+  test('first example not overlapped', () {
+    expect(notOverlapped("""
+#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2"""), 3);
+  });
+
+  test('dataset overlaps', () {
     expect(findOverlaps(day3()),107663);
+  });
+
+  test('dataset not overlapped', () {
+    expect(notOverlapped(day3()),1166);
   });
 
 }
