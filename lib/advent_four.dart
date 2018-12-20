0b6b678aec5478096258e912da96d0cfcda0b365
Jason Hite <hite77@gmail.com>
2018-12-19 21:20:36 -0500

    Day 4 part one and two

--- lib/advent_four.dart
+++ lib/advent_four.dart
@@ -1,8 +1,120 @@
+class GuardData {
+  int id;
+  int totalMinutes = 0;
+  List<int> minutes = new List.filled(60, 0);
+
+  fillMinutes(int start, int end) {
+    for(int i=start; i<end; i++) {
+      minutes[i] += 1;
+      totalMinutes += 1;
+    }
+  }
+
+  getMostMinutes() {
+    int minute=0;
+    int value=0;
+    for(int i=0; i<60; i++) {
+      if (minutes[i] > value) {
+        minute = i;
+        value = minutes[i];
+      }
+    }
+    return minute;
+  }
+}
+
+
 sort(String activity) {
   List<String> items = new List();
-  for (final item in activity.split('\n')) {
-    items.add(item);
-  }
+  activity.split('\n').forEach((item) => items.add(item));
   items.sort();
   return items;
 }
+
+GuardData findGuardWithMostTime(List<GuardData> data) {
+  int index=0;
+  int value=0;
+  for(int i=0; i<data.length; i++) {
+    if (data[i].totalMinutes > value) {
+      index = i;
+      value = data[i].totalMinutes;
+    }
+  }
+  return data[index];
+}
+
+int findGuardById(List<GuardData> data, int id) {
+  for(int i=0; i<data.length; i++) {
+    if (id == data[i].id) {
+      return i;
+    }
+  }
+  data.add(GuardData());
+  data[data.length-1].id = id;
+  return data.length-1;
+}
+
+day4_1solve(String activity) {
+  List<GuardData> data = parseAndSetupGuardData(activity);
+
+  GuardData guard = findGuardWithMostTime(data);
+  return (guard.getMostMinutes()*guard.id);
+}
+
+day4_2solve(String activity) {
+  List<GuardData> data = parseAndSetupGuardData(activity);
+
+  int minute=0;
+  int times=0;
+  int index=0;
+
+  for(int i=0; i<data.length; i++) {
+    int currentMinute = data[i].getMostMinutes();
+    int currentTimes = data[i].minutes[currentMinute];
+    if (currentTimes > times) {
+      times = currentTimes;
+      minute = currentMinute;
+      index=i;
+    }
+  }
+  return (data[index].id*minute);
+}
+
+List<GuardData> parseAndSetupGuardData(String activity) {
+  List<String> items = sort(activity);
+  List<GuardData> data = new List();
+  int currentGuard=0;
+  int startMinute=0;
+  int endMinute=0;
+
+  for(int i=0; i<items.length; i++) {
+    String item = items[i];
+    if (item.contains('Guard')) {
+      currentGuard = parseGuard(item);
+    } else if (item.contains('falls')) {
+      startMinute = parseMinute(item);
+    } else if (item.contains('wakes up')) {
+      endMinute = parseMinute(item);
+      int selected = findGuardById(data, currentGuard);
+      data[selected].fillMinutes(startMinute, endMinute);
+    }
+  }
+  return data;
+}
+
+parseMinute(String string) {
+  return int.parse(string.split(':')[1].split(']')[0]);
+}
+
+parseGuard(String string) {
+  return int.parse(string.split('Guard #')[1].split(' begins')[0]);
+}
+
+
+// sum of guard minutes asleep
+// list of minutes with duplicate count
+// for guard with most sleep, pick the minute most repeated
+
+// only minute portion is valid
+// return minute by guard #
+// [ Guard #, minutes asleep, [[minute #, times],] ]
--- test/advent_four_test.dart
+++ test/advent_four_test.dart
@@ -3,6 +3,29 @@ import 'package:flutter_app/advent_four.dart';
 import './day_data.dart';
 
 
+//day4
+
+example() {
+  return """
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up""";
+}
+
 void main() {
   test('sorts correctly', () {
     expect(sort("""
@@ -20,4 +43,12 @@ void main() {
 ]
     );
   });
+
+  test('example result', () {
+    expect(day4_1solve(example()),240);
+    expect(day4_2solve(example()),4455);
+  });
+
+  print('day4_1:'+day4_1solve(day4()).toString());
+  print('day4_2:'+day4_2solve(day4()).toString());
 }
